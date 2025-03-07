933207a89fbdb5c040ef5f17287e2102b57110b1
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2025-03-07 23:14:54 +0800

    删除开源组件数据模型

--- src/JiuLing.Platform.Repositories/Contexts/AppDbContext.cs
+++ src/JiuLing.Platform.Repositories/Contexts/AppDbContext.cs
@@ -1,11 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace JiuLing.Platform.Repositories.Contexts;
+﻿namespace JiuLing.Platform.Repositories.Contexts;
 
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<AppBase> AppBase { get; set; } = null!;
     public DbSet<AppRelease> AppRelease { get; set; } = null!;
-    public DbSet<Component> Component { get; set; } = null!;
     public DbSet<ConfigBase> ConfigBase { get; set; } = null!;
 }
