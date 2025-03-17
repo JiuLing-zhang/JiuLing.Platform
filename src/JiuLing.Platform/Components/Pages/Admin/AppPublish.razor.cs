57ebfd700b4235b6f32b143fb860d85bf99502a6
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2025-03-17 17:53:33 +0800

    迁移软件发布页面

---
+++ src/JiuLing.Platform/Components/Pages/Admin/AppPublish.razor.cs
@@ -0,0 +1,137 @@
+﻿using System.Security.Cryptography;
+using JiuLing.CommonLibs;
+using JiuLing.Platform.Common;
+using JiuLing.Platform.Components.Common;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using MudBlazor;
+
+namespace JiuLing.Platform.Components.Pages.Admin;
+
+public partial class AppPublish(
+    AuthenticationStateProvider authenticationStateProvider,
+    NavigationManager navigation,
+    IWebHostEnvironment webHostEnvironment,
+    IAppService appService,
+    IDialogService dialog
+    )
+{
+    private List<AppDetailDto>? _apps;
+    private readonly AppPublishDto _model = new();
+    private IBrowserFile? _file;
+
+    private MudForm _form = null!;
+
+    private bool _isPublishing;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        if (RendererInfo.IsInteractive)
+        {
+            var authState = await ((CustomAuthenticationStateProvider)authenticationStateProvider).GetAuthenticationStateAsync();
+            var user = authState.UserInfo();
+
+            if (user == null || user.Role != UserRoleEnum.Admin)
+            {
+                navigation.NavigateTo("/u/login", true);
+            }
+
+            await GetAppsAsync();
+        }
+    }
+
+    private async Task GetAppsAsync()
+    {
+        _apps = await appService.GetAppNamesAsync();
+    }
+    private async Task PublishAsync()
+    {
+        await _form.Validate();
+        if (!_form.IsValid)
+        {
+            return;
+        }
+
+        try
+        {
+            _isPublishing = true;
+
+            if (!Version.TryParse(_model.VersionName, out _))
+            {
+                await dialog.ShowInfoAsync("版本号格式不正确");
+                return;
+            }
+
+            if (!await appService.AllowPublishAsync(_model.AppKey, _model.Platform, _model.VersionName))
+            {
+                await dialog.ShowInfoAsync("版本号不能小于历史版本");
+                return;
+            }
+
+            var random = RandomUtils.GetOneByLength(4);
+            var fileName = $"{_model.AppKey}_{_model.Platform}_{_model.VersionName}_{random}".ToLower();
+
+            var fileExtension = Path.GetExtension(_file!.Name);
+            fileName = $"{fileName}{fileExtension}";
+
+            var relativePath = $"{GlobalConfig.AppFolder}/{fileName}";
+            var directory = Path.Combine(webHostEnvironment.ContentRootPath, "wwwroot", GlobalConfig.AppFolder);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+            var path = Path.Combine(directory, fileName);
+
+            HashAlgorithm hashAlgorithm;
+            switch (_model.SignType)
+            {
+                case SignTypeEnum.MD5:
+                    hashAlgorithm = MD5.Create();
+                    break;
+                case SignTypeEnum.SHA1:
+                    hashAlgorithm = SHA1.Create();
+                    break;
+                case SignTypeEnum.None:
+                default:
+                    await dialog.ShowInfoAsync("错误的签名方式");
+                    return;
+            }
+
+            string signValue;
+            await using FileStream fs = new(path, FileMode.Create);
+            using (hashAlgorithm)
+            {
+                var fileStream = _file.OpenReadStream(long.MaxValue);
+                await fileStream.CopyToAsync(fs);
+
+                var fileHash = await hashAlgorithm.ComputeHashAsync(fileStream);
+                signValue = Convert.ToHexStringLower(fileHash);
+            }
+
+            var model = new AppReleaseDto()
+            {
+                AppKey = _model.AppKey,
+                Platform = _model.Platform,
+                VersionName = _model.VersionName,
+                IsMinVersion = _model.IsMinVersion,
+                UpgradeLog = _model.Log,
+                FilePath = relativePath,
+                FileLength = (int)_file.Size,
+                SignType = _model.SignType,
+                SignValue = signValue
+            };
+            var result = await appService.PublishAsync(model);
+            if (result)
+            {
+                await dialog.ShowInfoAsync("发布成功");
+                return;
+            }
+            await dialog.ShowInfoAsync("发布失败");
+        }
+        finally
+        {
+            _isPublishing = false;
+        }
+    }
+}
